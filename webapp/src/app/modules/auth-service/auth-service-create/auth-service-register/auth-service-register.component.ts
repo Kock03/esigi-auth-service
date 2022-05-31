f5d173f31dbfecfe4e6fa1321da938f46568a4a6
biancanilsen <biancanilsen.entra21@gmail.com>
2022-05-31 17:45:43 -0300

    form

--- webapp/src/app/modules/auth-service/auth-service-create/auth-service-register/auth-service-register.component.ts
+++ webapp/src/app/modules/auth-service/auth-service-create/auth-service-register/auth-service-register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-service-register',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-service-register.component.scss']
 })
 export class AuthServiceRegisterComponent implements OnInit {
+  @Input('form') collaboratorProfileForm!: FormGroup;
+  @Output() onChange: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.collaboratorProfileForm = this.fb.group({
+      office: [null, Validators.required],
+      admissionDate: [null, Validators.required],
+      active: [null, Validators.required],
+      ddd: [null, Validators.required],
+      phoneNumber: [null, Validators.required],
+    });
+  }
+
+  next() {
+    this.onChange.next(true);
   }
 
 }

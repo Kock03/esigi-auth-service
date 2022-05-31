ca8278a34e71c2fd36f06b6fbe4281230c3ca3a5
davikrig-ha <daviluia2@gmail.com>
2022-05-31 11:41:31 -0300

    router

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'autorizacao',
+    loadChildren: () =>
+      import('./modules/auth-service/auth-service-create/auth-service.module').then(
+        m => m.AuthServiceModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webapp/src/app/modules/auth-service/auth-service-create/auth-service.module.ts
+++ webapp/src/app/modules/auth-service/auth-service-create/auth-service.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthServiceCreateComponent } from './auth-service-create.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthServiceListComponent } from './auth-service-list/auth-service-list.component';
 
-
+const routes: Routes = [
+ {
+   path: '/lista', 
+   component: AuthServiceListComponent
+ }
+]
 
 
 @NgModule({
   declarations: [
-    AuthServiceCreateComponent
+    AuthServiceCreateComponent,
+    AuthServiceListComponent
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
   ]
 })
 export class AuthServiceModule { }

f683a97a236684f014e8e33fb3bbe57caf58250d
davikrig-ha <daviluia2@gmail.com>
2022-09-27 10:30:09 -0300

    auth token

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { UserService } from 'src/services/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,7 @@ export class AppComponent {
   sidenav!: MatSidenav;
 
   openTree: boolean = false;
+  token!: string;
   compare!: any
   autorization: string = 'autorizacao'
 
@@ -40,6 +42,13 @@ export class AppComponent {
     this.router.events
       .pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((res: any) => {
+        let valid = res.url.indexOf('validate');
+        if (valid === -1) {
+        this.token = localStorage.getItem('token')!;
+          if (!this.token) {
+            location.replace(environment.portal);
+          }
+        }
         this.activeMenu = res.url.split('/')[1];
       });
   }
@@ -81,7 +90,7 @@ export class AppComponent {
   }
 
   openApp(port: number): void {
-    location.replace(`http://localhost:${port}`);
+    location.replace(`http://localhost:${port}/validate/${this.token}`);
   }
 
   logout(): void {

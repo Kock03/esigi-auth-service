67bd426b08b281b124d5d2924046566b5a3a6ba1
amandaestherbonatti <amandaestherbonatti@gmail.com>
2022-10-11 08:57:35 -0300

    reparos finalizados

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   COLLABORATOR_MS: '192.168.8.16:3501/api/v1/',
   AUTHORIZATION_MS: '192.168.8.16:3507/api/v1/',
-  port: '192.168.8.16',
+  port: '192.168.8.16:',
   message: '',
   protocol: 'http',
 
